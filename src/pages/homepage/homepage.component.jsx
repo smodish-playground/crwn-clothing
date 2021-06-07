973e3d1a2954b1aade311ff7c195428f6a6f31a6
Steve Smodish <smodish@hotmail.com>
2021-06-07 17:49:36 -0400

    homepage broken into components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Directory from '../../components/directory/directory.component'
+
+import './homepage.styles.scss'
+
+function HomePage() {
+  return (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  )
+}
+
+export default HomePage
