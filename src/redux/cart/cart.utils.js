a2db45335eccf2a3b2be9de8146e0792edcbed29
Steve Smodish <smodish@hotmail.com>
2021-06-26 23:03:12 -0400

    add checkout page quantity and remove functionality

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,11 +1,23 @@
-export const addItemToCart = (cartItems, cartitemToAdd) => {
-  const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartitemToAdd.id)
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToAdd.id)
 
   if (existingCartItem) {
     return cartItems.map((cartItem) =>
-      cartItem.id === cartitemToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
+      cartItem.id === cartItemToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
     )
   }
 
-  return [...cartItems, { ...cartitemToAdd, quantity: 1 }]
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+}
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToRemove.id)
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id)
+  }
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id ? { ...cartItem, quantity: cartItem.quantity - 1 } : cartItem
+  )
 }
