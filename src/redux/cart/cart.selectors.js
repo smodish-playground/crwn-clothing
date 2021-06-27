3cdd234d6951c585bf3a9be17b999a6c719806bc
Steve Smodish <smodish@hotmail.com>
2021-06-26 22:22:07 -0400

    add checkout page and checkout item component

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -8,4 +8,10 @@ export const selectCartItemsCount = createSelector([selectCartItems], (cartItems
   cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
 )
 
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.length
+    ? cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity * cartItem.price, 0)
+    : null
+)
+
 export const selectCartHidden = createSelector([selectCart], (cart) => cart.hidden)
