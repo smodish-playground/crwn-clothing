2d932ac6ba3afec642a0f0d6df59a8236064fd7b
Steve Smodish <smodish@hotmail.com>
2021-07-15 18:25:14 -0400

    add spinner omponent and set shop to conditionally render depending on loading status

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 
+import WithSpinner from '../../components/with-spinner/with-spinner.component'
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 import CollectionPage from '../collection/collection.component'
 
@@ -9,7 +10,18 @@ import { firestore, convertCollectionSnapshotToMap } from '../../firebase/fireba
 
 import { updateCollections } from '../../redux/shop/shop.actions'
 
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview)
+const CollectionPageWithSpinner = WithSpinner(CollectionPage)
+
 class ShopPage extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      loading: true
+    }
+  }
+
   unsubscribeFromSnapshot = null
 
   componentDidMount() {
@@ -20,14 +32,16 @@ class ShopPage extends React.Component {
       const collectionsMap = convertCollectionSnapshotToMap(snapshot)
       updateCollections(collectionsMap)
     })
+    this.setState({ loading: false })
   }
 
   render() {
     const { match } = this.props
+    const { loading } = this.state
     return (
       <div className='shop-page'>
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
-        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+        <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />} />
+        <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isloading={loading} {...props} />} />
       </div>
     )
   }
